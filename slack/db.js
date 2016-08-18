68d1af1a6508706d303b5c7fc90e0af80e52fa4b
Shuvo Ahmed <shuvoahmed@hotmail.com>
2016-08-18 13:15:34 -0400

    added service calls

--- slack/db.js
+++ slack/db.js
@@ -30,33 +30,33 @@ try {
 if (!dbexists) {
     db.serialize(function() {
         var createUserTableSql = "CREATE TABLE IF NOT EXISTS USER " +
-                       "(ID             INT         PRIMARY KEY     NOT NULL," +
-                       " USERNAME       CHAR(50)                    NOT NULL, " +
-                       " NAME           CHAR(50)                    NOT NULL, " +
-                       " EMAIL          CHAR(50)                    NOT NULL, " +
-                       " PASSWORD       CHAR(50)                    NOT NULL)"; 
+                       "(ID             INTEGER         PRIMARY KEY     AUTOINCREMENT NOT NULL," +
+                       " USERNAME       CHAR(50)                                      NOT NULL, " +
+                       " NAME           CHAR(50)                                      NOT NULL, " +
+                       " EMAIL          CHAR(50)                                      NOT NULL, " +
+                       " PASSWORD       CHAR(50)                                      NOT NULL)"; 
 
         var createTeamTableSql = "CREATE TABLE IF NOT EXISTS TEAM " +
-                       "(ID             INT         PRIMARY KEY     NOT NULL," +
-                       " TEAMNAME       CHAR(50)                    NOT NULL)";
+                       "(ID             INTEGER         PRIMARY KEY     AUTOINCREMENT NOT NULL," +
+                       " TEAMNAME       CHAR(50)                                      NOT NULL)";
 
         var createChannelTableSql = "CREATE TABLE IF NOT EXISTS CHANNEL " +
-                       "(ID             INT         PRIMARY KEY     NOT NULL," +
-                       " CHANNELNAME    CHAR(50)                    NOT NULL," +
-                       " TEAMID         INT                         NOT NULL," +
+                       "(ID             INTEGER         PRIMARY KEY     AUTOINCREMENT NOT NULL," +
+                       " CHANNELNAME    CHAR(50)                                      NOT NULL," +
+                       " TEAMID         INT                                           NOT NULL," +
                        " DESCRIPTION    TEXT                                )";
                        
         var createMessageTableSql = "CREATE TABLE IF NOT EXISTS MESSAGE " +
-                       "(ID             INT         PRIMARY KEY     NOT NULL," +
-                       " MESSAGE        TEXT                        NOT NULL, " + 
-                       " USERID         INT                         NOT NULL, " + 
-                       " CHANNELID      INT                         NOT NULL, " + 
-                       " DATE           TEXT                        NOT NULL)"; 
+                       "(ID             INTEGER         PRIMARY KEY     AUTOINCREMENT NOT NULL," +
+                       " MESSAGE        TEXT                                          NOT NULL, " + 
+                       " USERID         INT                                           NOT NULL, " + 
+                       " CHANNELID      INT                                           NOT NULL, " + 
+                       " DATE           TEXT                                          NOT NULL)"; 
 
         var createTeamMemberTableSql = "CREATE TABLE IF NOT EXISTS TEAMMEMBER " +
-                       "(ID             INT         PRIMARY KEY     NOT NULL," +
-                       " USERID         INT                         NOT NULL," +
-                       " TEAMID         INT                         NOT NULL)"; 
+                       "(ID             INTEGER         PRIMARY KEY     AUTOINCREMENT NOT NULL," +
+                       " USERID         INT                                           NOT NULL," +
+                       " TEAMID         INT                                           NOT NULL)"; 
 
         db.run(createUserTableSql);
         db.run(createTeamTableSql);
@@ -65,30 +65,55 @@ if (!dbexists) {
         db.run(createTeamMemberTableSql);
         
 
-        var insertUserSql = "INSERT INTO USER (ID, USERNAME, NAME, EMAIL, PASSWORD) " +
-            "VALUES (1, 'shuvo',   'Shuvo Ahmed',      'shuvoahmed@hotmail.com', 'shuvopassword')," +
-                   "(2, 'abu',     'Abu Moinuddin',    'abu@myslack.com',        'abupassword')," +
-                   "(3, 'charles', 'Charles Walsek',   'charles@myslack.com',    'charlespassword')," +
-                   "(4, 'beiying', 'Beiying Chen',     'beiying@myslack.com',    'beiyingpassword')," +
-                   "(5, 'swarup',  'Swarup Khatri',    'swarup@myslack.com',     'swarup');"; 
+        // var insertUserSql = "INSERT INTO USER (ID, USERNAME, NAME, EMAIL, PASSWORD) " +
+        //     "VALUES (1, 'shuvo',   'Shuvo Ahmed',      'shuvoahmed@hotmail.com', 'shuvopassword')," +
+        //            "(2, 'abu',     'Abu Moinuddin',    'abu@myslack.com',        'abupassword')," +
+        //            "(3, 'charles', 'Charles Walsek',   'charles@myslack.com',    'charlespassword')," +
+        //            "(4, 'beiying', 'Beiying Chen',     'beiying@myslack.com',    'beiyingpassword')," +
+        //            "(5, 'swarup',  'Swarup Khatri',    'swarup@myslack.com',     'swarup');"; 
 
-        var insertTeamSql = "INSERT INTO TEAM (ID, TEAMNAME) " +
-            "VALUES (1, 'slack')," +
-                   "(2, 'tweeter');"; 
+        var insertUserSql = "INSERT INTO USER (USERNAME, NAME, EMAIL, PASSWORD) " +
+            "VALUES ('shuvo',   'Shuvo Ahmed',      'shuvoahmed@hotmail.com', 'shuvopassword')," +
+                   "('abu',     'Abu Moinuddin',    'abu@myslack.com',        'abupassword')," +
+                   "('charles', 'Charles Walsek',   'charles@myslack.com',    'charlespassword')," +
+                   "('beiying', 'Beiying Chen',     'beiying@myslack.com',    'beiyingpassword')," +
+                   "('swarup',  'Swarup Khatri',    'swarup@myslack.com',     'swarup');"; 
 
-        var insertChannelSql = "INSERT INTO CHANNEL (ID, CHANNELNAME, TEAMID, DESCRIPTION) " +
-            "VALUES (1, 'slackChannel', 1, 'Channel for team slack')," +
-                   "(2, 'tweeterChannel', 2, 'Channel for team tweeter');"; 
+        // var insertTeamSql = "INSERT INTO TEAM (ID, TEAMNAME) " +
+        //     "VALUES (1, 'slack')," +
+        //            "(2, 'tweeter');"; 
+
+        var insertTeamSql = "INSERT INTO TEAM (TEAMNAME) " +
+            "VALUES ('slack')," +
+                   "('tweeter');"; 
+
+        // var insertChannelSql = "INSERT INTO CHANNEL (ID, CHANNELNAME, TEAMID, DESCRIPTION) " +
+        //     "VALUES (1, 'slackChannel', 1, 'Channel for team slack')," +
+        //            "(2, 'tweeterChannel', 2, 'Channel for team tweeter');"; 
+
+        var insertChannelSql = "INSERT INTO CHANNEL (CHANNELNAME, TEAMID, DESCRIPTION) " +
+            "VALUES ('slackChannel', 1, 'Channel for team slack')," +
+                   "('tweeterChannel', 2, 'Channel for team tweeter');"; 
         
-        var insertTeamMemberSql = "INSERT INTO TEAMMEMBER (ID, USERID, TEAMID) " +
-           "VALUES (1, 1, 1)," +
-                  "(2, 1, 2)," +
-                  "(3, 5, 1)," +
-                  "(4, 2, 2);";
+        // var insertTeamMemberSql = "INSERT INTO TEAMMEMBER (ID, USERID, TEAMID) " +
+        //    "VALUES (1, 1, 1)," +
+        //           "(2, 1, 2)," +
+        //           "(3, 5, 1)," +
+        //           "(4, 2, 2);";
 
-        var insertMessageSql = "INSERT INTO MESSAGE (ID, MESSAGE, USERID, CHANNELID, DATE) " +
-             "VALUES (1, 'Hi Swarup!',          1, 1, '2016-08-11 14:45:00'), " +
-                    "(2, 'Mocha testing....',   1, 2, '2016-08-05 12:46:00');";
+        var insertTeamMemberSql = "INSERT INTO TEAMMEMBER (USERID, TEAMID) " +
+           "VALUES (1, 1)," +
+                  "(1, 2)," +
+                  "(5, 1)," +
+                  "(2, 2);";
+
+        // var insertMessageSql = "INSERT INTO MESSAGE (ID, MESSAGE, USERID, CHANNELID, DATE) " +
+        //      "VALUES (1, 'Hi Swarup!',          1, 1, '2016-08-11 14:45:00'), " +
+        //             "(2, 'Mocha testing....',   1, 2, '2016-08-05 12:46:00');";
+
+        var insertMessageSql = "INSERT INTO MESSAGE (MESSAGE, USERID, CHANNELID, DATE) " +
+             "VALUES ('Hi Swarup!',          1, 1, '2016-08-11 14:45:00'), " +
+                    "('Mocha testing....',   1, 2, '2016-08-05 12:46:00');";
                     
         db.run(insertUserSql);
         db.run(insertTeamSql);
@@ -98,9 +123,9 @@ if (!dbexists) {
     });
 }
 
-// db.each("SELECT * FROM USER", function(err, row) {
-//     console.log(row.ID + " : " + row.USERNAME + " : " + row.NAME + " : " + row.EMAIL + " : " + row.PASSWORD);
-// });
+db.each("SELECT * FROM USER", function(err, row) {
+     console.log(row.ID + " : " + row.USERNAME + " : " + row.NAME + " : " + row.EMAIL + " : " + row.PASSWORD);
+});
 
 db.close();
 
@@ -113,9 +138,9 @@ function getChannels(conn, teamName) {
 }
 
 exports.createUser = createUser;
-function createUser(conn, id, username, name, email, password) {
+function createUser(conn, username, name, email, password) {
     //getUser(conn, 5);
-    var createUserSql = "INSERT INTO USER VALUES ("+ id + ", '" + username + "', '" + name + "', '" + email + "', '" + password + "');";
+    var createUserSql = "INSERT INTO USER (USERNAME, NAME, EMAIL, PASSWORD) VALUES (" + username + "', '" + name + "', '" + email + "', '" + password + "');";
     //console.log(createUserSql);
     //console.log(conn.run(createUserSql));
     conn.run(createUserSql);
